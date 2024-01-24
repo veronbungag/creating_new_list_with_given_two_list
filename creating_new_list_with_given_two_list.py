4d016dd282a9fdaf1ae643ebea58390698839dd3
veronbungag <rveron.bungag@gmail.com>
2024-01-24 21:00:38 +0800

    pseudocode

---
+++ creating_new_list_with_given_two_list.py
@@ -0,0 +1,9 @@
+#define a function for lists
+#create for loop
+#for loop first list
+#check if number is odd
+#store odd number to result list
+#for loop second list
+#check if number is even
+#store even number to result list
+#add given lists then print once checked
