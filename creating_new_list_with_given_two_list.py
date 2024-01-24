f8a023bf9f3d86225c40273a04743bf3325b6ddb
veronbungag <rveron.bungag@gmail.com>
2024-01-24 21:09:06 +0800

    for loop first list

--- creating_new_list_with_given_two_list.py
+++ creating_new_list_with_given_two_list.py
@@ -3,6 +3,7 @@ def merge_list(list1, list2):
     result_list = []
 #create for loop
 #for loop first list
+    for num in list1:
 #check if number is odd
 #store odd number to result list
 #for loop second list
